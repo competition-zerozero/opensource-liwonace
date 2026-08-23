19a84dd61c04be3bfaf64698b0af9cc5aeca1519
Huiyeongkim <huiyeong9619@naver.com>
2026-08-23 21:36:00 +0900

    first commit

---
+++ src/test/java/org/zerozero/opensource/OpensourceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerozero.opensource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpensourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
